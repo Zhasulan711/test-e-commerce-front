1d76542daed6f591c480bc499e2d14e0bc22df64
Zhasulan711 <zasulanmuratov52@gmail.com>
2026-01-05 16:46:59 +0500

    feat: add carts

--- src/features/components/home/api/index.ts
+++ src/features/components/home/api/index.ts
@@ -3,9 +3,5 @@ import { ProductCard } from "../types";
 export async function getAllProducts(): Promise<ProductCard[]> {
   const response = await fetch("https://fakestoreapi.com/products");
 
-  if (!response.ok) {
-    throw new Error("Failed to fetch products");
-  }
-
   return response.json();
 }
---
+++ src/pages/carts.tsx
@@ -0,0 +1,9 @@
+import { CartsSection } from "@/features/components/carts/components/CartsSection";
+
+export default function CartsPage() {
+  return (
+    <main className="mt-10 flex flex-col gap-5 mx-auto 2xl:max-w-325 xl:max-w-300 lg:max-w-220 md:max-w-172 sm:max-w-100 max-w-80">
+      <CartsSection />
+    </main>
+  );
+}
